8625c7c88682ac3abab4fa09b57b23a90b618ea7
hanna kebede <se.hanna.kebede@gmail.com>
2021-02-16 09:18:13 +0300

    update gallery

---
+++ asset/js/gallery.js
@@ -0,0 +1,10 @@
+var url = `https://picsum.photos/200/100`;
+var url2 = `https://picsum.photos/seed/picsum/200/100`;
+var url3 = `https://picsum.photos/200/100/?blur=2`;
+        
+var image = document.getElementById('img');
+var image2 = document.getElementById('img2');
+var image3 = document.getElementById('img3');
+image.src = url;
+image2.src = url2;
+image3.src = url3;
